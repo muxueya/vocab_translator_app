257c7f56e68f31469c560cf8ab5ace430de926cb
QianXingyan <qianxingyan@QianXingyandeMacBook-Pro.local>
2025-07-06 04:39:52 +0200

    updated app icon

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from app.main_window import TranslatorApp
+from PyQt5.QtGui import QIcon
+import os
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    icon_path = os.path.join(os.path.dirname(__file__), 'icon.png')
+    app.setWindowIcon(QIcon(icon_path))
     window = TranslatorApp()
     window.show()
     sys.exit(app.exec_())
